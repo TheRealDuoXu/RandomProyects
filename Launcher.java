627de81430dc00a5096bcfeecd55f12febf03139
DevDo <duoxu1987@gmail.com>
2021-01-15 21:56:04 +0100

    Add files via upload

--- Launcher.java
+++ Launcher.java
@@ -1,11 +1,9 @@
 package simpleATM;
 import java.util.Scanner;
-
 public class Launcher {
-static String name = "undefined"; static int id; static char seleccion; static boolean exitFlag=false;
+static boolean exitFlag=false;
 //estáticos, ya que no se crean ningun objeto Launcher
 // solo se utilizan sin referencia, por lo que son estáticos y solo existen dentro de main
-
 // inicializacion opciones
 final static char OP_CURRENT_BALANCE = 'A';
 final static char OP_WITHDRAW = 'B';
@@ -14,59 +12,67 @@ final static char OP_INTEREST = 'D';
 final static char OP_EXIT = 'E';
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
+		
 		System.out.print("Hello and welcome to FirstBank"
 				+ "\n please create your demo account"
 				+ "\n ------------------------------"
 				+ "\n your name: ");
-		name = input.next();
+		String name = input.next();
 		System.out.print("\n thanks " + name + 
 				"\n assign yourself an id:");
-		id = input.nextInt();
+		int id = input.nextInt();
+		assert input.hasNextLine();
+		input.nextLine();
 		System.out.print("\n creating your account");
 		User account = new User(name,id);
-		seleccion = account.menu(); // creates a local copy of selection, account.currentSelection cannot be used as it's private
-		
+		char option = account.menu();
 		while (!exitFlag) {
-			switch(seleccion) {
+			switch(option) {
 			case OP_CURRENT_BALANCE:
 				System.out.println("Current balance " + account.balance);
 				break;
 			case OP_WITHDRAW:
 				System.out.println("intitiating withdraw"
 						+ "\n How much? --- ");
-				float withdraw;
-				withdraw = input.nextFloat();
+				Float withdraw = input.nextFloat();
 				account.cashOut(withdraw);
 				break;
 			case OP_CASH_IN:
 				System.out.println("\n How much? --- ");
 				float cash = input.nextFloat();
+				input.nextLine();
 				account.cashIn(cash);
 				System.out.println("Current balance " + account.balance);
 				break;
 			case OP_INTEREST:
 				System.out.println("Interest calculator!!!! "
-						+ "\n Use current balance? Y/N" + account.balance);
-				char useBalance = input.next().toUpperCase().charAt(0);
+						+ "\n Use current balance? Y/N " + account.balance);
+				assert input.hasNextLine();
+				char useBalance = input.nextLine().toUpperCase().charAt(0);
 						if(useBalance == 'N') {//use balance no
 							System.out.println("Input balance");
 							float yourBalance = input.nextFloat();
+							input.nextLine();
 							System.out.println("Input years");
 							int years = input.nextInt();
+							input.nextLine();
 							System.out.println("Input interest");
 							float interest = input.nextFloat();
+							input.nextLine();
 							float result = account.interestCalc(years, interest,yourBalance);
-							System.out.println(result);
+							System.out.println("For " + yourBalance + " in " + years + " years, at " + interest +" interest rate "
+									+ "will yield " +  result);
 						}else { // use balance yes
 							System.out.println("you will be calculating the interest for the current balance" + account.balance);
 							System.out.println("please input years: ");
 							int years = input.nextInt();
+							input.nextLine();
 							System.out.println("Please input your interest rate: ");
 							float interest = input.nextFloat();
+							input.nextLine();
 							float result = account.interestCalc(years, interest);
-							System.out.println("For " + account.balance + "in " + years + "years, at " + interest+" interest rate "
+							System.out.println("For " + account.balance + " in " + years + " years, at " + interest +" interest rate "
 									+ "will yield " +  result);
 						}
 				break;
@@ -83,9 +89,14 @@ final static char OP_EXIT = 'E';
 				}catch(Exception e) {
 					System.out.println("Exception has occured" + e);
 				}
+				
+			} // outside switch, select again
+			if(!exitFlag) { // switch cae aqui, o si no no se puede romper el bucle
+			System.out.println("What else do you need? " +  account.name + "\n");
+			option = account.menu();
 			}
 		}
-		
+		input.close();
 	}
 
 }
--- User.java
+++ User.java
@@ -2,18 +2,18 @@ package simpleATM;
 import java.util.Scanner;
 
 public class User {
-	int id,currentBalance;
+	int id;
+	float balance=-1;
 	String name;
-	private char currentSelection='n';
-	private boolean menuNOTdone = true;
-	float balance=0;
+	static Scanner input = new Scanner(System.in);
+
 User(String name,int id){
 		this.id=id;
 		this.name= name;
 	}
 char menu() {
-	Scanner input = new Scanner(System.in);
-	
+	char currentSelection;
+	boolean menuNOTdone = true; // NO ALMACENA INFORMACION, ES UN CONTROLADOR DE SUCESOS
 	do {
 	System.out.println("");
 	System.out.println("Welcome to the FirstBank");
@@ -25,23 +25,31 @@ char menu() {
 	System.out.println("------------------------ ");
 	System.out.println("Select... ");
 	// currentSelection will be passed to the while loop as a condition to break
-	currentSelection = input.next().toUpperCase().charAt(0);
+	currentSelection = input.nextLine().toUpperCase().charAt(0);
+	input.nextLine();
 	char pool[] = {'A','B','C','D','E'};
 	for(char x : pool) {
-		if(x != currentSelection) {
-			menuNOTdone = false;
-			}else {
-				menuNOTdone = true;
-			}
+		switch (x) {
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E': 
+			menuNOTdone = false; // menu is done
+			break;
+		default:
+			menuNOTdone = true; // menu is not done
+			System.out.println("A -> E moron!!!");
+			break;
+		}
 		}
 	}while(menuNOTdone);
-	input.close();
 	return currentSelection;
 }//end menu()
 
 void cashIn(float cash) {
-	balance += cash;
-	System.out.print(cash + "  correctly handed in");
+	balance += (cash+1); //error extraño hace que sea una unidad mas pequena
+	System.out.print(cash + "  correctly handed in \n");
 }
 
 void cashOut(float withdraw){
@@ -68,55 +76,66 @@ float interestCalc(int years,float interest) {
 	float result = balance;
 	//check years and interest are correctly inputed
 	// create local scanner for input
-	Scanner input = new Scanner(System.in);
 	while(years <= 0 && interest <= 0) {
-		if (years < 0) {
+		if (years <= 0) {
 			System.out.println("The number of years must be an integer greater than 0"
 					+ " \n your input -" + years);
 			years = input.nextInt();
+			input.nextLine();
 		}
 		if (interest <= 0) {
 			System.out.println("The interest rate must be a float greater than 0"
 					+ " \n your input -" + interest);
 			interest = input.nextFloat();
+			input.nextLine();
 		}
 	}// checked starting conditions, leasing input
-	input.close();
-	System.out.println("Everything has been setup correctly" + years + " years, " + interest + " interest"
-			+ "\n applying standart inflation rate 2.0%" );
+	System.out.println("Everything has been setup correctly " + years + " years, " + interest + " interest"
+			+ "\n aplaying standart inflation rate 2.0%" );
 	interest -= 2;
+	interest /= 100;
 	// calculate interest by for loop
-	for (int i=3;i<years;i++) { // condicion an = a1 * exponent(r (n-1)), no quería buscar la funcion en Math
-	System.out.println("year: " + (i-2) + "balance: " + result);
-		result = balance * interest;
-	}
+	for (int i = 0;i<years;i++)  // condicion an = a1 * exponent(r (n-1)), no quería buscar la funcion en Math
+		{ 
+		System.out.println("year: " + (i+1) + "balance: " + result);
+		result = result * interest; //interest is in percentage
+		}
 	
-	return result;}
-
+	return result;
 	
-float interestCalc (int years, float interest, float yourBalance) {
+}
+float interestCalc(int years, float interest,float yourBalance) {
 	float result = yourBalance;
-	Scanner input = new Scanner(System.in);
+	//check years and interest are correctly inputed
+	// create local scanner for input
 	while(years <= 0 && interest <= 0) {
-		if (years < 0) {
+		if (years <= 0) {
 			System.out.println("The number of years must be an integer greater than 0"
 					+ " \n your input -" + years);
 			years = input.nextInt();
+			input.nextLine();
 		}
 		if (interest <= 0) {
 			System.out.println("The interest rate must be a float greater than 0"
 					+ " \n your input -" + interest);
 			interest = input.nextFloat();
+			input.nextLine();
 		}
-	input.close();
-	System.out.println("Everything has been setup correctly" + years + " years, " + interest + " interest" + yourBalance + "balance"
-			+ "\n applying standart inflation rate 2.0%" );
+	}// checked starting conditions, leasing input
+	System.out.println("Everything has been setup correctly " + years + " years, " + interest + " interest"
+			+ "\n aplaying standart inflation rate 2.0%"
+			+ "\n you have chosen: " + yourBalance );
 	interest -= 2;
+	interest /= 100;
+	interest += 1;
 	// calculate interest by for loop
-	for (int i=3;i<years;i++) { // condicion an = a1 * exponent(r (n-1)), no quería buscar la funcion en Math
-	System.out.println("year: " + (i-2) + "balance: " + result);
-		result = yourBalance * interest;
-	}
-	}
-	return result;}
+	for (int i = 0;i<years;i++)  // condicion an = a1 * exponent(r (n-1)), no quería buscar la funcion en Math
+		{ 
+		System.out.println("year: " + (i+1) + "balance: " + result);
+		result = result * interest; //interest is in percentage
+		}
+	
+	return result;
+}
+
 }// class User ends
